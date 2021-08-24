f6c06eb62255b964cb66a3da82034ce78e0aacbf
Simon <simonwaigi@outlook.com>
2021-08-24 05:33:31 +0300

    exec renamed in schedule method

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from read_email_schedule_task import execute
 import schedule
 import time
 
-schedule.every().day.at("05:23").do(exec)
+schedule.every().day.at("05:30").do(execute)
 
 while True:
     schedule.run_pending()
